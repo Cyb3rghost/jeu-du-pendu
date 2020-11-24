2560a94d8db2da7ea01ea676550916a87c6a901b
Cyb3rghost <cyb3rghostx@gmail.com>
2020-11-24 13:53:45 +0400

    Premier commit

---
+++ views/main.php
@@ -0,0 +1,43 @@
+<div class="container">
+
+    <div class="cadreAction mt-5">
+        <a class="btn btn-dark btn-block" id="nouvellePartie" href="#" role="button" style="margin-top: 25%;">Nouvelle partie !</a>
+    </div>
+
+    <div id="cadrePartie">
+
+        <div class="text-center">
+            <img id="statutPendu" src="assets/etape1.png" alt="">
+            <input type="hidden" id="etape" value="1"/>
+        </div>
+        <br/>
+        <br/>
+
+        <div class="text-center">
+            <button class="btn btn-dark" id="boutonA" value="A" type="submit">A</button>&nbsp;<button class="btn btn-dark" id="boutonB" value="B" type="submit">B</button>
+            <button class="btn btn-dark" id="boutonC" value="C" type="submit">C</button>&nbsp;<button class="btn btn-dark" id="boutonD" value="D" type="submit">D</button>
+            <button class="btn btn-dark" id="boutonE" value="E" type="submit">E</button>&nbsp;<button class="btn btn-dark" id="boutonF" value="F" type="submit">F</button>
+            <button class="btn btn-dark" id="boutonG" value="G" type="submit">G</button>&nbsp;<button class="btn btn-dark" id="boutonH" value="H" type="submit">H</button>
+            <button class="btn btn-dark" id="boutonI" value="I" type="submit">I</button>&nbsp;<button class="btn btn-dark" id="boutonJ" value="J" type="submit">J</button>
+            <button class="btn btn-dark" id="boutonK" value="K" type="submit">K</button>&nbsp;<button class="btn btn-dark" id="boutonL" value="L" type="submit">L</button>
+            <button class="btn btn-dark" id="boutonM" value="M" type="submit">M</button>&nbsp;<button class="btn btn-dark" id="boutonN" value="N" type="submit">N</button>
+            <button class="btn btn-dark" id="boutonO" value="O" type="submit">O</button>&nbsp;<button class="btn btn-dark" id="boutonP" value="P" type="submit">P</button>
+            <button class="btn btn-dark" id="boutonQ" value="Q" type="submit">Q</button>&nbsp;<button class="btn btn-dark" id="boutonR" value="R" type="submit">R</button>
+            <button class="btn btn-dark" id="boutonS" value="S" type="submit">S</button>&nbsp;<button class="btn btn-dark" id="boutonT" value="T" type="submit">T</button>
+            <button class="btn btn-dark" id="boutonU" value="U" type="submit">U</button>&nbsp;<button class="btn btn-dark" id="boutonV" value="V" type="submit">V</button>
+            <button class="btn btn-dark" id="boutonW" value="W" type="submit">W</button>&nbsp;<button class="btn btn-dark" id="boutonX" value="X" type="submit">X</button>
+            <button class="btn btn-dark" id="boutonY" value="Y" type="submit">Y</button>&nbsp;<button class="btn btn-dark" id="boutonZ" value="Z" type="submit">Z</button>
+        </div>
+        <br/>
+        <h1 class="text-center motAleatoire"></h1>
+        <div class="text-center">
+            <h1>
+                <div class="contenu"></div>
+            </h1>
+        </div>
+        <p>Début partie !</p>
+
+
+    </div>
+
+</div>
